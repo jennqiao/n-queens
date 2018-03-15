64dafb30d9459900398e02d1974077e3880b985d
jennqiao <jennifercqiao@gmail.com>
2018-03-15 10:23:31 -0700

    pass test for findNRooks

--- src/solvers.js
+++ src/solvers.js
@@ -16,16 +16,8 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
-};
-
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-  
+  // return value
+  var solution = undefined;
   //declare solutions results array
   var solutions = [];
   //create initial board with n
@@ -58,12 +50,73 @@ window.countNRooksSolutions = function(n) {
         //untoggle 
         boardObj.togglePiece(rowIndex, colIndex);
       } else if (counter === n) {
+                // for deep copy of array
+        let solutionsBoard = JSON.parse(JSON.stringify(boardObj.rows()));
         //push board to solutions
-        solutions.push(boardObj.rows());
+        solutions.push(solutionsBoard);
         //untoggle 
         boardObj.togglePiece(rowIndex, colIndex);
       }       
-    }  
+    }
+  }
+  //loop through squares in initial board (only first row bc of optimization - )
+  for (var i = 0; i < n; i++) {
+    //call helper function (board, current square, counter)
+    solver(boardObj, 0, i, 0);
+    
+  }
+  solution = solutions[0];
+  console.log(solutions)
+  
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  
+  return solution;
+};
+
+// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+window.countNRooksSolutions = function(n) {
+  var solutionCount = undefined;  
+  //declare solutions results array
+  var solutions = [];
+  //create initial board with n
+  var boardObj = new Board({n:n}); //not the matrix
+  
+  var solver = function (boardObj, rowIndex, colIndex, counter) {
+    debugger;
+  //helper function (board, nextPieceCoordinates)
+    // togglePiece: function(rowIndex, colIndex) 
+    boardObj.togglePiece(rowIndex, colIndex);
+    //check for conflicts
+    //check for row and column conflicts
+    if (boardObj.hasAnyRowConflicts() || boardObj.hasAnyColConflicts()) {
+      //if conflicts, toggle piece back 
+      boardObj.togglePiece(rowIndex, colIndex);
+      //return (out of function)
+      return;
+    } else {
+      //increment counter
+      counter++;
+      //if counter < n
+      if (counter < n) {
+        //loop through next row //optimization:
+        for (var i = 0; i < n; i++) {
+          //call helper function on new potential piece (board, potentialPiece, counter+1)
+          //skip same column
+          if (i !== colIndex) {
+            solver(boardObj, rowIndex + 1, i, counter);
+          }
+        }
+        //untoggle 
+        boardObj.togglePiece(rowIndex, colIndex);
+      } else if (counter === n) {
+        // for deep copy of array
+        let solutionsBoard = JSON.parse(JSON.stringify(boardObj.rows()));
+        //push board to solutions
+        solutions.push(solutionsBoard);
+        //untoggle 
+        boardObj.togglePiece(rowIndex, colIndex);
+      }       
+    }
   };
   
   //loop through squares in initial board (only first row bc of optimization - )
@@ -72,8 +125,8 @@ window.countNRooksSolutions = function(n) {
     solver(boardObj, 0, i, 0);
     
   }
-
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  console.log('Number of solutions for ' + n + ' rooks:', solutions.length);
+  console.log(1, solutions)
   //return solutions array.length
   return solutions.length;
 };
